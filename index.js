37973bd990cf59fef605e845d3f2302777aff25b
Tomás Migone <tomasmigone@gmail.com>
2018-04-29 05:45:22 -0300

    + Changed to Alexa Smart Home skill. Discovery implemented

--- index.js
+++ index.js
@@ -1,14 +1,77 @@
-let alexa = require('alexa-app')
-let app = new alexa.app('sample')
+'use strict'
 
-app.intent('number', {
-  'slots': { 'number': 'AMAZON.NUMBER' },
-  'utterances': ['say the number {-|number}']
-},
-function (request, response) {
-  var number = request.slot('number')
-  response.say('You asked for the number ' + number)
-})
+const bgh = require('./lib/bgh')
+const Promise = require('bluebird')
 
-// connect the alexa-app to AWS Lambda
-exports.handler = app.lambda()
+exports.handler = (request, context, callback) => {
+  let directive = request.directive.header.name
+  switch (directive) {
+    case 'Discover': {
+      handleDiscovery(request, callback)
+      break
+    }
+
+    default: {
+      const errorMessage = `Directive not supported: ${directive}`
+      console.log('[ERROR] %s', errorMessage)
+      callback(new Error(errorMessage))
+    }
+  }
+}
+
+function handleDiscovery (request, callback) {
+  const userAccessToken = request.directive.payload.scope.token.trim()
+
+  getDevicesFromBGHService(userAccessToken).then((endpoint) => {
+    var payload = {
+      endpoints: [ endpoint ]
+    }
+    var header = request.directive.header
+    header.name = 'Discover.Response'
+    console.log('DEBUG', 'Discovery Response: ', JSON.stringify({ header: header, payload: payload }))
+    callback(null, { event: { header: header, payload: payload } })
+  })
+}
+
+function getDevicesFromBGHService (token) {
+  const device = new bgh.Device()
+  device.setToken(token)
+
+  return new Promise((resolve, reject) => {
+    device.enumHomes().then((data) => {
+      device.setHomeId(data[0].HomeID)
+      device.GetDataPacket().then((data) => {
+        let device = data.Devices[0]
+
+        let endpoint = {
+          endpointId: device.DeviceID,
+          manufacturerName: 'BGH',
+          friendlyName: device.Description,
+          description: 'BGH Smart Home device.',
+          displayCategories: ['THERMOSTAT'],
+          cookie: {},
+          capabilities: [{
+            type: 'AlexaInterface',
+            interface: 'Alexa.ThermostatController',
+            version: '3',
+            properties: {
+              supported: [
+                { 'name': 'lowerSetpoint' },
+                { 'name': 'targetSetpoint' },
+                { 'name': 'upperSetpoint' },
+                { 'name': 'thermostatMode' }
+              ],
+              proactivelyReported: false,
+              retrievable: true
+            }
+          }]
+        }
+        resolve(endpoint)
+      }).catch((e) => {
+        console.log(e)
+      })
+    }).catch((e) => {
+      console.log(e)
+    })
+  })
+}
